5d2627874a1ac744bce006ae7aa38f20e744a752
chun1 <arthur@chunai.dev>
2024-05-13 17:05:31 +1000

    switch to fire

--- echo360.py
+++ echo360.py
@@ -1,7 +1,9 @@
 import re
 import sys
 from echo360.main import main
+import fire
 
 if __name__ == "__main__":
-    sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
-    sys.exit(main())
+    # sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
+    # sys.exit(main())
+    fire.Fire(main)
