a822832c5eddaca02ccda9a47f67efb3d6b2ccc8
chun1 <arthur@chunai.dev>
2024-05-13 17:03:04 +1000

    remove useless files

--- echo360/utils.py
+++
@@ -1,6 +0,0 @@
-def naive_versiontuple(v):
-    """
-    This only works for version tuple with the same number of parts.
-    Expects naive_versiontuple('xx.yy.zz') < naive_versiontuple('aa.bb.cc').
-    """
-    return tuple(map(int, (v.split("."))))
