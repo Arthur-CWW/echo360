4d244510c33f082bec606c6f313d21e23cc4fe43
chun1 <arthur@chunai.dev>
2024-05-13 16:04:30 +1000

    cleanup code remove p2, add imports add types

--- echo360/course.py
+++ echo360/course.py
@@ -1,23 +1,27 @@
 import json
 import re
 import sys
+from typing import Optional
 
 import requests
 import selenium
 import logging
 
 from .videos import EchoVideos, EchoCloudVideos
+import selenium.common.exceptions
+import selenium.webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
 
 _LOGGER = logging.getLogger(__name__)
 
 
 class EchoCourse(object):
-    def __init__(self, uuid, hostname=None, alternative_feeds=False):
-        self._course_id = None
-        self._course_name = None
-        self._uuid = uuid
-        self._videos = None
-        self._driver = None
+    def __init__(self, uuid,driver:WebDriver, hostname=None, alternative_feeds=False):
+        self._course_id:Optional[str] = None
+        self._course_name:Optional[str] = None
+        self._uuid:str = uuid
+        self._videos:Optional[EchoVideos] = None
+        self.driver:WebDriver =driver
         self._alternative_feeds = alternative_feeds
         if hostname is None:
             self._hostname = "https://view.streaming.sydney.edu.au:8443"
@@ -25,7 +29,7 @@ class EchoCourse(object):
             self._hostname = hostname
 
     def get_videos(self):
-        if self._driver is None:
+        if self.driver is None:
             self._blow_up("webdriver not set yet!!!", "")
         if not self._videos:
             try:
@@ -33,7 +37,7 @@ class EchoCourse(object):
                 videos_json = course_data_json["section"]["presentations"][
                     "pageContents"
                 ]
-                self._videos = EchoVideos(videos_json, self._driver)
+                self._videos = EchoVideos(videos_json, self.driver)
             except KeyError as e:
                 self._blow_up(
                     "Unable to parse course videos from JSON (course_data)", e
@@ -53,13 +57,12 @@ class EchoCourse(object):
 
     @property
     def url(self):
-        return "{}/ess/portal/section/{}".format(self._hostname, self._uuid)
+        return f"{self._hostname}/ess/portal/section/{self._uuid}"
 
     @property
     def video_url(self):
-        return "{}/ess/client/api/sections/{}/section-data.json?pageSize=100".format(
-            self._hostname, self._uuid
-        )
+        return f"{self._hostname}/ess/client/api/sections/{self._uuid}/section-data.json?pageSize=100"
+
 
     @property
     def course_id(self):
@@ -79,30 +82,23 @@ class EchoCourse(object):
                     "Unable to parse course id (e.g. CS473) from JSON (course_data)", e
                 )
 
-        if type(self._course_id) != str:
-            # it's type unicode for python2
-            return self._course_id.encode("utf-8")
         return self._course_id
 
     @property
     def course_name(self):
         if self._course_name is None:
-            # trigger getting course_id to get course name as well
             self.course_id
-        if type(self._course_name) != str:
-            # it's type unicode for python2
-            return self._course_name.encode("utf-8")
         return self._course_name
 
-    @property
-    def driver(self):
-        if self._driver is None:
-            self._blow_up("webdriver not set yet!!!", "")
-        return self._driver
+    # @property
+    # def driver(self):
+    #     if self._driver is None:
+    #         self._blow_up("webdriver not set yet!!!", "")
+    #     return self._driver
 
     @property
     def nice_name(self):
-        return "{0} - {1}".format(self.course_id, self.course_name)
+        return f"{self.course_id} - {self.course_name}"
 
     def _get_course_data(self):
         try:
@@ -110,7 +106,7 @@ class EchoCourse(object):
             _LOGGER.debug(
                 "Dumping course page at %s: %s",
                 self.video_url,
-                self._driver.page_source,
+                self.driver.page_source,
             )
             json_str = self.driver.find_element_by_tag_name("pre").text
         except ValueError as e:
@@ -119,11 +115,11 @@ class EchoCourse(object):
         return self.course_data
 
     def set_driver(self, driver):
-        self._driver = driver
+        self.driver = driver
 
     def _blow_up(self, msg, e):
         print(msg)
-        print("Exception: {}".format(str(e)))
+        print(f"Exception: {str(e)}")
         sys.exit(1)
 
 
@@ -132,14 +128,14 @@ class EchoCloudCourse(EchoCourse):
         super(EchoCloudCourse, self).__init__(*args, **kwargs)
 
     def get_videos(self):
-        if self._driver is None:
+        if self.driver is None:
             raise Exception("webdriver not set yet!!!", "")
         if not self._videos:
             try:
                 course_data_json = self._get_course_data()
                 videos_json = course_data_json["data"]
                 self._videos = EchoCloudVideos(
-                    videos_json, self._driver, self.hostname, self._alternative_feeds
+                    videos_json, self.driver, self.hostname, self._alternative_feeds
                 )
             # except KeyError as e:
             #     print("Unable to parse course videos from JSON (course_data)")
@@ -152,7 +148,7 @@ class EchoCloudCourse(EchoCourse):
 
     @property
     def video_url(self):
-        return "{}/section/{}/syllabus".format(self._hostname, self._uuid)
+        return f"{self._hostname}/section/{self._uuid}/syllabus"
 
     @property
     def course_id(self):
@@ -198,12 +194,12 @@ class EchoCloudCourse(EchoCourse):
             _LOGGER.debug(
                 "Dumping course page at %s: %s",
                 self.video_url,
-                self._driver.page_source,
+                self.driver.page_source,
             )
             # use requests to retrieve data
             session = requests.Session()
             # load cookies
-            for cookie in self._driver.get_cookies():
+            for cookie in self.driver.get_cookies():
                 session.cookies.set(cookie["name"], cookie["value"])
 
             r = session.get(self.video_url)
